ec9f23f995ecaf6f9686c7095830f8ad276b21dc
Bishwendu Kundu <bishwenduk029@gmail.com>
2023-01-08 12:47:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import torch
+from torch import autocast
+from diffusers import DiffusionPipeline, EulerDiscreteScheduler
+import base64
+from io import BytesIO
+import os
+
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
+def init():
+    global model
+    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
+    
+    repo_id = "stabilityai/stable-diffusion-2"
+
+    scheduler = EulerDiscreteScheduler.from_pretrained(repo_id, subfolder="scheduler", prediction_type="v_prediction")
+
+    model = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, revision="fp16", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN).to("cuda")
+
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
+
+    # Parse out your arguments
+    prompt = model_inputs.get('prompt', None)
+    height = model_inputs.get('height', 768)
+    width = model_inputs.get('width', 768)
+    num_inference_steps = model_inputs.get('num_inference_steps', 50)
+    guidance_scale = model_inputs.get('guidance_scale', 7.5)
+    input_seed = model_inputs.get("seed",None)
+    
+    #If "seed" is not sent, we won't specify a seed in the call
+    generator = None
+    if input_seed != None:
+        generator = torch.Generator("cuda").manual_seed(input_seed)
+    
+    if prompt == None:
+        return {'message': "No prompt provided"}
+    
+    # Run the model
+    with autocast("cuda"):
+        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator).images[0]
+    
+    buffered = BytesIO()
+    image.save(buffered,format="JPEG")
+    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    # Return the results as a dictionary
+    return {'image_base64': image_base64}
---
+++ download.py
@@ -0,0 +1,29 @@
+# In this file, we define download_model
+# It runs during container build time to get model weights built into the container
+
+from diffusers import DiffusionPipeline, EulerDiscreteScheduler
+import torch
+
+import os
+
+def download_model():
+    # do a dry run of loading the huggingface model, which will download weights at build time
+    #Set auth token which is required to download stable diffusion model weights
+    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
+
+
+    repo_id = "stabilityai/stable-diffusion-2"
+
+    scheduler = EulerDiscreteScheduler.from_pretrained(repo_id, subfolder="scheduler", prediction_type="v_prediction")
+
+    model = DiffusionPipeline.from_pretrained(repo_id, 
+        torch_dtype=torch.float16, 
+        revision="fp16", 
+        scheduler=scheduler,
+        use_auth_token=HF_AUTH_TOKEN
+    )
+
+
+
+if __name__ == "__main__":
+    download_model()
